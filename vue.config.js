e9e80b5347a60a597c4ab158fd80bc4739168f59
Woshiajuana <zhigang.chen@owulia.com>
2019-08-30 16:04:32 +0800

    subcode

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+const resolve = dir => path.join(__dirname, dir);
+
+const vueConfig = {
+  productionSourceMap: false,
+  lintOnSave: false,
+    publicPath: './',
+    outputDir: 'dist',
+    assetsDir: 'static',
+  devServer: {
+    port: 8080,
+    https: false,
+    before: (app) => {
+      // eslint-disable-next-line global-require
+      require('./mock').mock(app);
+    },
+  },
+  baseUrl: '',
+  // crossorigin: 'anonymous',
+  // integrity: true,
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', resolve('src'));
+  },
+};
+
+
+module.exports = vueConfig;
